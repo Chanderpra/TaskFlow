a704f29c3562e069ef5edab7d0878744324bf812
Chander ios <chander.ios@vsmartinfotech.in>
2026-08-05 13:25:44 +0530

    format

--- TaskFlowTests/TaskFlowTests.swift
+++ TaskFlowTests/TaskFlowTests.swift
@@ -8,31 +8,15 @@
 import XCTest
 @testable import TaskFlow
 
+import XCTest
+@testable import TaskFlow
+
 final class TaskFlowTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testTaskCreation() {
+        let task = TaskItem.init(title: "Learn SwiftUI", category: "Learning", completed: false, date: "Today")
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        XCTAssertEqual(task.title, "Learn CI/CD")
+        XCTAssertFalse(task.completed)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
